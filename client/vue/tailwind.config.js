c4e73347c789a96a3b758d8fb362c206f5960a1a
asaf bar <assafbar81@gmail.com>
2024-03-04 12:45:27 +0200

    add tailwind to vue project

---
+++ client/vue/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  // remove unused style in production code
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
