ef873dc6e5ea739184b2ef7e498b0b25f1b1886f
asaf bar <assafbar81@gmail.com>
2024-03-04 14:59:44 +0200

    fix TS configuration handle tailwind warnings

--- client/vue/tailwind.config.js
+++ client/vue/tailwind.config.js
@@ -1,8 +1,7 @@
 // tailwind.config.js
 module.exports = {
   // remove unused style in production code
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
