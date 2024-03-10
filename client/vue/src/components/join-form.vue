6dabf12f1f642b6f48a43f59c2e946c2e9830bd9
asaf bar <assafbar81@gmail.com>
2024-03-10 10:42:00 +0200

    establish socket connections.  join form component with socket connection and client side storage

--- client/vue/src/components/join-form.vue
+++ client/vue/src/components/join-form.vue
@@ -1,4 +1,36 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { socket } from "../socket";
+import { userStore } from "../stores/user-store.ts";
+
+const username = ref("");
+
+const enterRoom = () => {
+  socket.emit("join room", { username: username.value });
+  userStore.userName = username.value;
+};
+
+let socketState = ref("");
+</script>
+
 <template>
-  <div>Join form</div>
+  <form
+    @submit.prevent="enterRoom"
+    class="flex w-full justify-center m-auto gap-1"
+  >
+    <p>{{ socketState.connected }}</p>
+    <input
+      type="text"
+      v-model="username"
+      required
+      placeholder="Your name"
+      class="max-w-[60%] gap-2 rounded-md w-1/4 px-1 focus:outline-none"
+    />
+    <button
+      type="submit"
+      class="rounded-md p-2 bg-blue-500 hover:bg-blue-600 transition-colors"
+    >
+      Join
+    </button>
+  </form>
 </template>
